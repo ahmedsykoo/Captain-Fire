4623d46209b1b2fc46cd8b9e8f3ad566a92ec26b
ahmedsykoo <ahmed.sykooo@gmail.com>
2026-08-13 16:52:54 +0300

    Add color definitions for theme

---
+++ app/src/main/java/com/fairkm/driver/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.fairkm.driver.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Emerald = Color(0xFF008577)
+val EmeraldLight = Color(0xFF55D6BE)
+
+val Amber = Color(0xFFFFA000)
+val AmberLight = Color(0xFFFFB74D)
+
+val GoodGreen = Color(0xFF1E9E6D)
+val BadRed = Color(0xFFD64545)
